d99d8bb74d96d089bb7fa9e9970f08e7424956f0
DanyilUA <122489621+DanyilUA@users.noreply.github.com>
2023-09-24 16:20:32 +0200

    v2 fixed

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-https://ibb.co/59kXkxC
-https://ibb.co/bKgJfps
-https://ibb.co/ZdSqYtY
-https://ibb.co/7QrmrFs
+https://ibb.co/album/0yS52N
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import * as contactService from "./db/contacts.js";
+import * as contactService from "./contacts.js";
 import yargs from "yargs";
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
       case 'list':
           const contactList = await contactService.getAllContacts();
-          return console.log(contactList);
+          return console.table(contactList);
 
       case 'get':
           const contactId = await contactService.contactGetById(id);
@@ -23,10 +23,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       console.warn('\x1B[31m Unknown action type!');
   }
 }
-// invokeAction({ action: 'list' });
-// invokeAction({ action: 'get', id: '05olLMgyVQdWRwgKfg5J6' });
-// invokeAction({ action: 'add', name: 'Mango', email: 'mango@gmail.com', phone: '322-22-22' });
-// invokeAction({ action: 'remove', id: '' });
+
 
 const { argv } = yargs(process.argv.slice(2));
 invokeAction(argv);
